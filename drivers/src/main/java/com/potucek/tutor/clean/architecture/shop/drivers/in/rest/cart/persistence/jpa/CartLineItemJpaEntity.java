deaba35703a7ac6f435243d0e804bf63ebcf1ad1
Vincent Potucek <vincent.potucek@udg.de>
2025-01-18 12:54:59 +0100

    wip

---
+++ drivers/src/main/java/com/potucek/tutor/clean/architecture/shop/drivers/in/rest/cart/persistence/jpa/CartLineItemJpaEntity.java
@@ -0,0 +1,25 @@
+package com.potucek.tutor.clean.architecture.shop.drivers.in.rest.cart.persistence.
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * JPA entity class for a shopping cart line item.
+ *
+ * @author Sven Woltmann
+ */
+@Entity
+@Table(name = "CartLineItem")
+@Getter
+@Setter
+public class CartLineItemJpaEntity {
+
+  @Id @GeneratedValue private Integer id;
+
+  @ManyToOne private CartJpaEntity cart;
+
+  @ManyToOne private ProductJpaEntity product;
+
+  private int quantity;
+}
