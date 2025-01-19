c6bef92c5c1a674d7d60f601a8ac8df4f75d54cf
Vincent Potucek <vincent.potucek@udg.de>
2025-01-19 17:22:00 +0100

    wip

--- drivers/application-services/domain-services/src/main/java/com/potucek/shop/drivers/adapters/application/business/rules/registry/HawkDomainService.java
+++ drivers/application-services/domain-services/src/main/java/com/potucek/shop/drivers/adapters/application/business/rules/registry/HawkDomainService.java
@@ -6,6 +6,15 @@ import java.util.Set;
 
 public interface HawkDomainService {
 
+    /**
+     * Find all hawks (eagles, buzzards, vultures, kites, etc), sort them according to their IUCN Category and print them (System.out
+     * .println). For each species you should print at least the English name, the scientific name and the IUCN category.
+     * <p>
+     * Sorting should be according to the list above with birds in the "LC" category printed first and birds in the "DD" category printed
+     * last.
+     * <p>
+     * You can find all hawks using the family name "Hawks, eagles"
+     */
     Set<Hawk> findAllHawksSortedByIUCNCategory();
 
 }
