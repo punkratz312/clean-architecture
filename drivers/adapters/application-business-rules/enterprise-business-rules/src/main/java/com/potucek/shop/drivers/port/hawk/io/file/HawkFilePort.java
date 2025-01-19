6a7f60e2a67a07d12e56ab03829b04c0c794d488
Vincent Potucek <vincent.potucek@udg.de>
2025-01-19 13:08:06 +0100

    wip

--- drivers/adapters/application-business-rules/enterprise-business-rules/src/main/java/com/potucek/shop/drivers/port/hawk/io/file/HawkFilePort.java
+++ drivers/adapters/application-business-rules/enterprise-business-rules/src/main/java/com/potucek/shop/drivers/port/hawk/io/file/HawkFilePort.java
@@ -1,13 +1,33 @@
 package com.potucek.shop.drivers.port.hawk.io.file;
 
+import com.potucek.shop.drivers.core.application.service.hawk.HawkApplicationService;
 import com.potucek.shop.drivers.port.hawk.HawkPort;
+import lombok.SneakyThrows;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Set;
 
 public class HawkFilePort implements HawkPort {
+
+    private final HawkApplicationService hawkApplicationService;
+
+    public HawkFilePort(HawkApplicationService hawkApplicationService) {
+        this.hawkApplicationService = hawkApplicationService;
+    }
+
+    @SneakyThrows
+//    @PostContruct
+    public void init() {
+        List<String> file = Files.readAllLines(Path.of(""));
+        registerHawks(Set.copyOf(file));
+    }
+
+
     @Override
     public Set<String> registerHawks() {
-        return Set.of();
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -17,6 +37,6 @@ public class HawkFilePort implements HawkPort {
 
     @Override
     public void registerHawk(Set<String> hawk) {
-
+        throw new UnsupportedOperationException();
     }
 }
--- drivers/adapters/application-business-rules/enterprise-business-rules/src/main/java/com/potucek/shop/drivers/port/hawk/web/rest/HawkResourcePort.java
+++ drivers/adapters/application-business-rules/enterprise-business-rules/src/main/java/com/potucek/shop/drivers/port/hawk/web/rest/HawkResourcePort.java
@@ -5,18 +5,25 @@ import com.potucek.shop.drivers.port.hawk.HawkPort;
 import java.util.Set;
 
 public class HawkResourcePort implements HawkPort {
+
+    private final HawkPort hawkPort;
+
+    public HawkResourcePort(HawkPort hawkPort) {
+        this.hawkPort = hawkPort;
+    }
+
     @Override
     public Set<String> registerHawks() {
-        return Set.of();
+        return hawkPort.registerHawks();
     }
 
     @Override
     public void registerHawks(Set<String> hawks) {
-        throw new IllegalStateException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void registerHawk(Set<String> hawk) {
-        throw new IllegalStateException();
+        throw new UnsupportedOperationException();
     }
 }
