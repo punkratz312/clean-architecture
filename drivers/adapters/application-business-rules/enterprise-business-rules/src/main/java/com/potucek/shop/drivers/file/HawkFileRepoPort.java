0bca102404a9d6f05fd0140db095a9efb382c34e
Vincent Potucek <vincent.potucek@udg.de>
2025-01-19 15:14:56 +0100

    wip

--- drivers/adapters/application-business-rules/enterprise-business-rules/src/main/java/com/potucek/shop/drivers/file/HawkFileRepoPort.java
+++ drivers/adapters/application-business-rules/enterprise-business-rules/src/main/java/com/potucek/shop/drivers/file/HawkFileRepoPort.java
@@ -12,10 +12,12 @@ import java.util.stream.Collectors;
 
 public class HawkFileRepoPort implements HawkRepoPort {
 
+    public static final String HAWKS_EAGLES = "Hawks, eagles;";
+
     @SneakyThrows
     @Override
     public Set<String> findAllHawksSortedByIUCNCategory() {
         TreeSet<String> strings = new TreeSet<>(Files.readAllLines(Path.of("")));
-        return strings.stream().sorted(String::compareTo).collect(Collectors.toCollection(LinkedHashSet::new));
+        return strings.stream().filter(s -> s.startsWith(HAWKS_EAGLES)).sorted(String::compareTo).collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
