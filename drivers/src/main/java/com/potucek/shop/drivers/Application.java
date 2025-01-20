a357759c9ffc08a7b94f42f40f91534e89530162
Vincent Potucek <vincent.potucek@udg.de>
2025-01-20 16:01:10 +0100

    wip

--- drivers/src/main/java/com/potucek/shop/drivers/Application.java
+++ drivers/src/main/java/com/potucek/shop/drivers/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.potucek.shop")
 @AutoConfiguration
-class Application {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
