7b8dc9e7fb5780272ecf5e341b5406301caa93b0
Vincent Potucek <vincent.potucek@udg.de>
2025-02-10 18:54:16 +0100

    wip

---
+++ drivers/src/main/java/package-info.java
@@ -0,0 +1,8 @@
+@InfrastructureLayer
+@org.springframework.modulith.ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package com.potucek.shop.drivers;
+
+import org.jmolecules.architecture.layered.InfrastructureLayer;
+import org.springframework.modulith.ApplicationModule;
