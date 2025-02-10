9dd3a42d51842105a57e4c884e6dfefa95213070
Vincent Potucek <vincent.potucek@udg.de>
2025-02-10 17:00:38 +0100

    wip

--- drivers/core/application-services/domain-services/entities/src/main/java/com/potucek/shop/drivers/core/application/services/domain/services/entities/hawk/Hawk.java
+++ drivers/core/application-services/domain-services/entities/src/main/java/com/potucek/shop/drivers/core/application/services/domain/services/entities/hawk/Hawk.java
@@ -1,5 +1,10 @@
 package com.potucek.shop.drivers.core.application.services.domain.services.entities.hawk;
 
+import org.jmolecules.ddd.annotation.AggregateRoot;
+import org.jmolecules.ddd.annotation.ValueObject;
+
+@AggregateRoot
+@ValueObject
 public record Hawk(
         String birdFamily,
         String englishBirdName,
--- drivers/src/test/java/com/potucek/shop/drivers/web/rest/hawk/JMoleculesDddUnitTest.java
+++ drivers/src/test/java/com/potucek/shop/drivers/web/rest/hawk/JMoleculesDddUnitTest.java
@@ -3,12 +3,24 @@ package com.potucek.shop.drivers.web.rest.hawk;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 import org.jmolecules.archunit.JMoleculesDddRules;
+import org.junit.jupiter.api.Test;
 
+//@AnalyzeClasses(packagesOf = Application.class)
 @AnalyzeClasses(packages = "com.potucek.shop")
 class JMoleculesDddUnitTest {
     @ArchTest
     void whenCheckingAllClasses_thenCodeFollowsAllDddPrinciples(JavaClasses classes) {
         JMoleculesDddRules.all().check(classes);
+    }
+
+    @ArchTest
+    ArchRule ddd = JMoleculesDddRules.all();
+
+    @Test
+    void documentation() {
+//        new Documenter(Application.class)
+//                .writeDocumentation(Options.defaults(), CanvasOptions.defaults());
     }
 }
